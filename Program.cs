0283ddf2fc76a4cdbd5c367ff99356e827df5dcf
Alexander Vasilenko <vasilenkosaha4321@gmail.com>
2023-01-30 18:31:25 +0600

    вторая задача

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("input your first number: ");
+﻿/*Console.Write("input your first number: "); // 1 задача
 int numberA = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("input your second number: ");
@@ -19,3 +19,32 @@ else
 }
 Console.WriteLine($"Самое маленькое число : {min}");
 Console.WriteLine($"Самое большое число : {max}");
+*/
+
+// задача 2
+/*
+Console.Write( "Введите первое число : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write( "Введите первое число : ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write( "Введите первое число : ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(numberA < numberB)
+{
+    max = numberB;
+}
+else 
+{
+    max = numberA;
+}
+if(max < numberC)
+{
+    max = numberC;
+}
+Console.WriteLine($"Самое большое число : {max}");
+*/
