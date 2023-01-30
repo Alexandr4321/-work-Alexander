0e3aa4bb14ee5471104a7a4926528cb91cb6ee61
Alexander Vasilenko <vasilenkosaha4321@gmail.com>
2023-01-30 18:05:45 +0600

    первая задача

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("input your first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input your second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+ 
+int min = 0 ;
+int max = 0 ;
+
+if(numberA < numberB)
+{
+     min = numberA;
+     max = numberB;
+}
+else
+{
+     min = numberB;
+     max = numberA;
+}
+Console.WriteLine($"Самое маленькое число : {min}");
+Console.WriteLine($"Самое большое число : {max}");
